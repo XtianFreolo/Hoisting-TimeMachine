fe382463baad6abc4bb9fa6a215d659da5c1ffc0
XtianFreolo <F60@umbc.edu>
2025-04-15 21:57:49 -0400

    Finish activity

--- time-travel.js
+++ time-travel.js
@@ -1,7 +1,7 @@
 /* Task 1: Declare a Destination Variable */
 
 let destination = Ancient Egypt;
-console.log(destination);
+console.log(`Destination: ${destination}`);
 
 // TODO: Use `let` to declare a variable named `destination` and assign it the value `"Ancient Egypt"`. Print the destination to the console.
 
@@ -12,19 +12,29 @@ console.log(destination);
 
 
 let destination = "Medieval Europe";
-console.log(destination);
+console.log(`New destination: ${destination}`);
 
 
 /* Task 3: Declare a Constant Travel Date */
 // TODO: Use `const` to declare a variable named `travelDate` and set it to `"2024-03-15"`. Try to change the `travelDate` to another value and observe and explain what happens as a comment.
+
+const travelDate = "2024-03-15";
+console.log(`Travel Date: ${travelDate}`);
+
+
 /*
  * Observations:
  * TODO: Explain here.
- */
+ // cannot be changed, its a const 
 
 /* Task 4: Experiment with Variable Hoisting */
 // TODO: Before declaring any variable, try to print a variable named `timeMachineModel` to the console. Then, declare `timeMachineModel` using `var` and assign it the value `"T-800"`. Observe and explain what happens as a comment.
-/*
- * Observations:
+
+console.log(`Time Machine Model: ${timeMachineModel}`);
+var timeMachineModel = "T-800";
+
+
+/* Observations:
  * TODO: Explain here.
  */
+//its undefined. because of hoisting, the variable is declared but not initialized yet.
