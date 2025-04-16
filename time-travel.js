8e1c1a9d67274d73cea0b15b2af1fe2d5ae04600
XtianFreolo <F60@umbc.edu>
2025-04-15 21:46:49 -0400

    task 2 answered

--- time-travel.js
+++ time-travel.js
@@ -3,11 +3,17 @@
 let destination = Ancient Egypt;
 console.log(destination);
 
-
 // TODO: Use `let` to declare a variable named `destination` and assign it the value `"Ancient Egypt"`. Print the destination to the console.
+
+
 
 /* Task 2: Change the Destination */
 // TODO: Now, change the `destination` variable to `"Medieval Europe"`. Print the new destination to the console.
+
+
+let destination = "Medieval Europe";
+console.log(destination);
+
 
 /* Task 3: Declare a Constant Travel Date */
 // TODO: Use `const` to declare a variable named `travelDate` and set it to `"2024-03-15"`. Try to change the `travelDate` to another value and observe and explain what happens as a comment.
